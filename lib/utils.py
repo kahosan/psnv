6fc8e191963937665f7c2fe8e10ae31f3c3d40cb
kahosan <15116355657l@gmail.com>
2024-09-25 16:35:36 +0800

    fix(download): 403

--- lib/utils.py
+++ lib/utils.py
@@ -23,9 +23,12 @@ def check_folder_exists(path: str):
 def download_file(path: str, url: str):
     import requests
 
-    with requests.get(
-        url, headers={"Referer": "https://app-api.pixiv.net/"}, stream=True
-    ) as r:
+    headers = {
+        "Referer": "https://app-api.pixiv.net/",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+    }
+
+    with requests.get(url, headers=headers, stream=True) as r:
         r.raise_for_status()
         with open(path, "wb") as file:
             for chunk in r.iter_content(chunk_size=8192):
